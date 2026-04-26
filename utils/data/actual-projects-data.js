8090229202ba96b33ad87c1b1bb92c09c8c8f94a
Caozzz1611 <beritozambrano@gmail.com>
2026-04-26 13:09:18 -0500

    Actualizaciones

--- utils/data/actual-projects-data.js
+++ utils/data/actual-projects-data.js
@@ -5,8 +5,8 @@ export const actualProjectsData = [
     description: 'BizTrack es un sistema de información diseñado para la gesstión de inventario, pedidos y ventas de un almacen en Abastos, optimizando muchos procesos manuales y mejorando la eficiencia del negocio.',
     cover_image: '/proyectos/biztrackmockup.png',
     url: '#',
-    published_at: new Date().toISOString(),
-    tools: ['PHP', 'MySQL', 'HTML', 'CSS', 'JavaScript'],
+    published_at: '2024-11-16',
+    tools: ['PHP', 'MySQL', 'HTML', 'CSS', 'JavaScript', 'Git'],
   },
   {
     id: 2,
@@ -14,8 +14,8 @@ export const actualProjectsData = [
     description: 'FitControl es una plataforma SaaS diseñada para gestionar clubes deportivos y escuelas de fútbol, permitiendo administrar jugadores, entrenamientos, pagos, asistencia y rendimiento desde un solo sistema de forma eficiente y organizada.',
     cover_image: '/proyectos/fitcontrolmockup.png',
     url: '#',
-    published_at: new Date().toISOString(),
-    tools: ['Laravel', 'PostgreSql', 'React'],
+    published_at: 'En progreso',
+    tools: ['Laravel', 'PostgreSql', 'React', 'Filament', 'Tailwind', 'Livewire', 'Docker', 'Redis', 'Git'],
   },
 
 ];
